f1a174f2e7a98b4906fb2cee00caaf62a9a1e130
Duy Tan <tan.nguyenngocduy112@gmail.com>
2025-04-16 12:37:21 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      // { find: '~', replacement: '/src' },
+      { find: '@boards', replacement: '/src/pages/boards' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@auth', replacement: '/src/pages/auth' },
+      { find: '@ultils', replacement: '/src/ultils' },
+      { find: '@assets', replacement: '/src/assets' },
+      { find: '@api', replacement: '/src/api' },
+    ]
+  }
+})
